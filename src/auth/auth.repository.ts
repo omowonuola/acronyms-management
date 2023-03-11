173024b60d8d6849c172386cc739304abe1e2fb4
omowonuola <oyedmola@gmail.com>
2023-03-11 12:11:06 +0000

    adds jwt payload to the authentication api

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -10,7 +10,7 @@ import { AuthEntity } from './auth.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
-// import { JwtPayload } from './jwt-payload.interface';
+import { JwtPayload } from './jwt-payload';
 
 export class AuthRepository {
   private readonly logger = new Logger(AuthRepository.name);
@@ -42,16 +42,14 @@ export class AuthRepository {
     }
   }
 
-  async signInUser(
-    authCredentialsDto: AuthCredentialsDto,
-  ): Promise<{ accessToken: string }> {
+  async signInUser(authCredentialsDto: AuthCredentialsDto): Promise<any> {
     const { username, password } = authCredentialsDto;
     const user = await this.userEntity.findOne({ where: { username } });
 
     if (user && (await bcrypt.compare(password, user.password))) {
       const payload: JwtPayload = { username };
       const accessToken: string = await this.jwtService.sign(payload);
-      return { accessToken };
+      return { username, accessToken };
     } else {
       throw new UnauthorizedException('Please check your login credentials');
     }
